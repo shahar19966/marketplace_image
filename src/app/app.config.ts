49fe4d73cadc4820464ad6102f52bf1fc4de3de3
tomer953 <tomer953@gmail.com>
2023-07-27 14:20:37 +0300

    update app config to support provideWith syntax

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,24 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {ToastrModule} from 'ngx-toastr';
+import {  provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom([BrowserAnimationsModule, 
-    ToastrModule.forRoot({
-      "closeButton": true,
-      "progressBar": true,
-      "positionClass": "toast-bottom-left",
-    })])]};
+  providers: [
 
+    // app routes
+    provideRouter(routes),
+
+
+    provideAnimations(),
+
+    // ngx-toastr
+    provideToastr({
+      closeButton: true,
+      progressBar: true,
+      positionClass: 'toast-bottom-left',
+    }),
+  ],
+};
