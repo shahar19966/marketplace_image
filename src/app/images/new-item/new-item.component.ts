ff94dd17a034ed96c3d77196862e13e5443147e0
tomer953 <tomer953@gmail.com>
2023-07-27 14:40:29 +0300

    Fix add image redirect bug

--- src/app/images/new-item/new-item.component.ts
+++ src/app/images/new-item/new-item.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ImageSrvice } from '../image.service';
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,26 +16,34 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-new-item',
   standalone: true,
-  imports: [CommonModule,InputTextModule,ButtonModule ,FormsModule,ReactiveFormsModule ],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './new-item.component.html',
-  styleUrls: ['./new-item.component.scss']
+  styleUrls: ['./new-item.component.scss'],
 })
 export class NewItemComponent {
   form!: FormGroup;
   hasUnsavedChanges = false;
 
-  constructor(private formBuilder: FormBuilder,
-    private imageService:ImageSrvice,
+  constructor(
+    private formBuilder: FormBuilder,
+    private imageService: ImageSrvice,
     private toastr: ToastrService,
     private router: Router,
-    private route: ActivatedRoute,) {}
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.form = this.formBuilder.group({
       title: ['', Validators.required],
       artist: ['', Validators.required],
       imageURL: ['', Validators.required],
-      price: ['', Validators.required]
+      price: ['', Validators.required],
     });
 
     this.form.valueChanges.subscribe(() => {
@@ -40,10 +54,8 @@ export class NewItemComponent {
   onSubmit() {
     this.hasUnsavedChanges = false;
     const { title, artist, imageURL, price } = this.form.value;
-    this.imageService.addNewImage(title,imageURL,artist,price);
-    this.toastr.success("Image added successfully");
-    this.router.navigate(['images-list'],{relativeTo: this.route});
-
+    this.imageService.addNewImage(title, imageURL, artist, price);
+    this.toastr.success('Image added successfully');
+    this.router.navigateByUrl('/');
   }
-
 }
