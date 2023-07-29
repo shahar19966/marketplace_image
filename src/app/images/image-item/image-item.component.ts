309b9ace82feb42b290e28d0cd1c61f21bee68d9
SHAHARS3 <shahars3@rafael.co.il>
2023-07-29 12:56:39 +0300

    add logic for delete image

--- src/app/images/image-item/image-item.component.ts
+++ src/app/images/image-item/image-item.component.ts
@@ -4,6 +4,7 @@ import { ButtonModule } from 'primeng/button';
 import { Image } from 'src/app/interface/image.interface';
 import { ShoppingCardService } from 'src/app/shopping-list-item/shopping-card.service';
 import { ToastrService } from 'ngx-toastr';
+import { ImageSrvice } from '../image.service';
 
 @Component({
   selector: 'app-image-item',
@@ -17,6 +18,7 @@ export class ImageItemComponent {
 
   private toastr = inject(ToastrService);
   private shoppingCartService = inject(ShoppingCardService);
+  private imageService = inject(ImageSrvice);
 
   onAddToCart() {
     if (this.image) {
@@ -24,6 +26,18 @@ export class ImageItemComponent {
       this.toastr.success('Image added successfully');
     } else {
       this.toastr.error('Image is undefined, cannot add to cart.');
+    }
+  }
+  onDeleteButton() {
+    let userResponse = window.confirm(
+      'Are you sure you want to delete this Image?'
+    );
+
+    if (userResponse && this.image) {
+      this.imageService.deleteImage(this.image);
+      this.toastr.success('Image delete successfully');
+    } else {
+      this.toastr.error('Image is undefined, cannot remove from the list.');
     }
   }
 }
