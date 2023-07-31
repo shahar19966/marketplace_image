e55778b6ac8b09db8de0319b5be457c4f58b827f
tomer953 <tomer953@gmail.com>
2023-07-31 14:51:43 +0300

    required input for image item

--- src/app/images/image-item/image-item.component.ts
+++ src/app/images/image-item/image-item.component.ts
@@ -14,26 +14,23 @@ import { ImageService } from '../image.service';
   styleUrls: ['./image-item.component.scss'],
 })
 export class ImageItemComponent {
-  @Input() image: Image | undefined;
+  @Input({ required: true }) image!: Image;
 
   private toastr = inject(ToastrService);
   private shoppingCartService = inject(ShoppingCardService);
   private imageService = inject(ImageService);
 
-  onAddToCart() {
-    if (this.image) {
-      this.shoppingCartService.addItemToCart(this.image);
-      this.toastr.success('Image added successfully');
-    } else {
-      this.toastr.error('Image is undefined, cannot add to cart.');
-    }
+  addToCart() {
+    this.shoppingCartService.addItemToCart(this.image);
+    this.toastr.success('Image added successfully');
   }
-  onDeleteButton() {
+
+  deleteItem() {
     let userResponse = window.confirm(
       'Are you sure you want to delete this Image?'
     );
 
-    if (userResponse && this.image) {
+    if (userResponse) {
       this.imageService.deleteImage(this.image);
       this.toastr.success('Image delete successfully');
     }
